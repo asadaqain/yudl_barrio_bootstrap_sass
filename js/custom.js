f15cdf3bf1853a66ac84c6797a97c617a75d439a
Ali Sadaqain <sadaqain@yorku.ca>
2023-01-12 10:52:19 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.yudl_barrio_bootstrap_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
